76eab71fb51b0b638d94a715eba1acb795a3ccab
RicardoCorrea <ricardocorreabautista@gmail.com>
2020-06-16 12:18:50 -0500

    continuara..

--- src/index.js
+++ src/index.js
@@ -1,45 +1,56 @@
-var http = require("http");
-var { info, error } = require("./modules/my-log");
-var { countries } = require("countries-list");
-var url = require("url");
-var querystring = require("querystring");
 
-var server = http.createServer(function (request, response) {
-  var parser = url.parse(request.url);
-  var pathname = parser.pathname;
-  var query = querystring.parse(parser.query);
-  console.log('var query',query);
+const { info, error } = require("./modules/my-log");
+const { countries } = require("countries-list");
+const express = require("express");
 
-  if (pathname === "/") {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write("<html><body><p>Homa page</p></body></html>");
-    response.end();
-  } else if (pathname === "/hola") {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write(
-      "<html><body><p>a perro me estas saludando, hola wey</p></body></html>"
-    );
-    response.end();
-  } else if (pathname === "/countries") {
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.write(JSON.stringify(countries[query.country]));
-    response.end();
-  } else if (pathname === "/info") {
-    var rurl = info(pathname);
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write(rurl);
-    response.end();
-  } else if (pathname === "/error") {
-    var rurl = error(pathname);
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write(rurl);
-    response.end();
-  } else {
-    response.writeHead(404, { "Content-Type": "text/html" });
-    response.write("<html><body><p>not fund</p></body></html>");
-  }
+const app = express();
+
+app.get('/',function(request,response){
+  response.status(200).send("hola");
 });
 
-server.listen(3000);
+app.get('*',function(request,response){
+  response.status(404).send("Page Not Found");
+});
 
-console.log("running on 4000");
+// var server = http.createServer(function (request, response) {
+//   var parser = url.parse(request.url);
+//   var pathname = parser.pathname;
+//   var query = querystring.parse(parser.query);
+//   console.log("var query", query);
+
+//   if (pathname === "/") {
+//     response.writeHead(200, { "Content-Type": "text/html" });
+//     response.write("<html><body><p>Homa page</p></body></html>");
+//     response.end();
+//   } else if (pathname === "/hola") {
+//     response.writeHead(200, { "Content-Type": "text/html" });
+//     response.write(
+//       "<html><body><p>a perro me estas saludando, hola wey</p></body></html>"
+//     );
+//     response.end();
+//   } else if (pathname === "/countries") {
+//     response.writeHead(200, { "Content-Type": "application/json" });
+//     response.write(JSON.stringify(countries[query.country]));
+//     response.end();
+//   } else if (pathname === "/info") {
+//     var rurl = info(pathname);
+//     response.writeHead(200, { "Content-Type": "text/html" });
+//     response.write(rurl);
+//     response.end();
+//   } else if (pathname === "/error") {
+//     var rurl = error(pathname);
+//     response.writeHead(200, { "Content-Type": "text/html" });
+//     response.write(rurl);
+//     response.end();
+//   } else {
+//     response.writeHead(404, { "Content-Type": "text/html" });
+//     response.write("<html><body><p>not fund</p></body></html>");
+//   }
+// });
+
+app.listen(5000,function(){
+  console.log("running on http://127.0.0.1:5000");
+
+});
+
