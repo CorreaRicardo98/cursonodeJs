a5252c357be2f36d75733d1edb05c9227e0266e7
RicardoCorrea <ricardocorreabautista@gmail.com>
2020-06-16 22:22:21 -0500

    continua aún......

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const countries = require('countries-list');
+const { countries, languages } = require('countries-list');
 
 const app = express();
 
@@ -8,53 +8,28 @@ app.get('/', (request, response) => {
 });
 
 app.get('/country', (request, response) => {
-  const { lang } = request.query;
   // eslint-disable-next-line no-console
-  console.log(lang);
-  response.json(countries[request.query.lang]);
+  console.log(request.query);
+  response.send(JSON.stringify(countries[request.query.code]));
+});
+
+app.get('/languages/:lang', (request, response) => {
+  const lang = languages[request.params.lang];
+
+  if (lang) {
+    response.json(lang);
+  } else {
+    response.status(400).send('NOT FOUND');
+  }
+  // eslint-disable-next-line no-console
+  console.log(request.params);
 });
 
 app.get('*', (request, response) => {
   response.status(404).send('Page Not Found');
 });
 
-// var server = http.createServer(function (request, response) {
-//   var parser = url.parse(request.url);
-//   var pathname = parser.pathname;
-//   var query = querystring.parse(parser.query);
-//   console.log("var query", query);
-
-//   if (pathname === "/") {
-//     response.writeHead(200, { "Content-Type": "text/html" });
-//     response.write("<html><body><p>Homa page</p></body></html>");
-//     response.end();
-//   } else if (pathname === "/hola") {
-//     response.writeHead(200, { "Content-Type": "text/html" });
-//     response.write(
-//       "<html><body><p>a perro me estas saludando, hola wey</p></body></html>"
-//     );
-//     response.end();
-//   } else if (pathname === "/countries") {
-//     response.writeHead(200, { "Content-Type": "application/json" });
-//     response.write(JSON.stringify(countries[query.country]));
-//     response.end();
-//   } else if (pathname === "/info") {
-//     var rurl = info(pathname);
-//     response.writeHead(200, { "Content-Type": "text/html" });
-//     response.write(rurl);
-//     response.end();
-//   } else if (pathname === "/error") {
-//     var rurl = error(pathname);
-//     response.writeHead(200, { "Content-Type": "text/html" });
-//     response.write(rurl);
-//     response.end();
-//   } else {
-//     response.writeHead(404, { "Content-Type": "text/html" });
-//     response.write("<html><body><p>not fund</p></body></html>");
-//   }
-// });
-
-app.listen(8000, () => {
+app.listen(10000, () => {
   // eslint-disable-next-line no-console
-  console.log('running on http://127.0.0.1:8000');
+  console.log('running on http://127.0.0.1:10000');
 });
